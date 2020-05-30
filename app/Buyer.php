3640ac5b7831ee2c328c1781a07dde6011b03039
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-05-30 17:12:17 +0800

    Add buyers api

--- app/Buyer.php
+++ app/Buyer.php
@@ -7,6 +7,8 @@ use App\Transaction;
 
 class Buyer extends User
 {
+    protected $table = 'users';
+    
     public function transactions(){
         return $this->hasMany(Transaction::class);
     }
