bd4c496393a9f83b49d31bb8133e841103a9bddf
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-05-30 15:17:09 +0800

    Add factory for seed

--- app/Seller.php
+++ app/Seller.php
@@ -7,6 +7,7 @@ use App\Product;
 
 class Seller extends User
 {
+    protected $table = 'users';
     public function products(){
         return $this->hasMany(Product::class);
     }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,11 @@
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use App\Category;
+use App\Product;
+use App\Transaction;
+use App\Buyer;
+use App\Seller;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +25,39 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verified != User::VERIFIED_USER ? User::generateVerificationCode() : null,
+        'admin' => $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER])
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id, //User::inRandomOrder()->first()->id
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1,3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
